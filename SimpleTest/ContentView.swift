4d17812fbe3ae01c54243859ba85a0af4d9adc26
Carlos Thurber B <thurber.carlos@gmail.com>
2020-07-19 21:46:12 -0500

    Initial Commit

---
+++ SimpleTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTest
+//
+//  Created by Carlos Thurber Boaventura on 19Jul20.
+//  Copyright © 2020 Carlos Thurber Boaventura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
